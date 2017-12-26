e642fdc004bc111ff1fc27fd6ca390122b8f2969
ktr0328 <zf7heatpa@gmail.com>
2017-12-26 13:57:33 +0900

    辞書動いた

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'natto'
 require './src/dictionary'
 
 def client
-  @client ||= Line::Bot::Client.new { |config|
+  @client ||= Line::Bot::Client.new {|config|
     config.channel_secret = ENV['CHANNEL_SECRET']
     config.channel_token = ENV['CHANNEL_ACCESS_TOKEN']
   }
@@ -16,47 +16,37 @@ post '/callback' do
   body = request.body.read
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
+    error 400 do
+      'Bad Request'
+    end
   end
 
   category = Dictionary::Categorize
   conversion = Dictionary::Conversion
 
   events = client.parse_events_from(body)
-  events.each { |event|
+  events.each {|event|
     if event['type'] == 'message' then
       if event['message']['type'] == 'text' then
         nm = Natto::MeCab.new
         text = event.message['text']
         conversion_text = ""
         nm.parse(text) do |n|
-         word = category[n.surface]
-         if word
-           conversion_text += conversion[word]
-         else
-           conversion_text += n.surface
-         end
-
-         if n.surface == '選定'
-           conversion_text += 'エクスカリバー'
-         elsif n.surface == '週末'
-           conversion_text += 'ラグナロク'
-         elsif n.surface == '僕' || n.surface == '俺' || n.surface == '私'
-           conversion_text += '我'
-         elsif n.surface == '理解できない'
-           conversion_text += 'エニグマ'
-         else
-           conversion_text += n.surface
-         end
+          word = category[n.surface]
+          if word
+            conversion_text += conversion[word]
+          else
+            conversion_text += n.surface
+          end
         end
 
-          message = [
+        message = [
             {
-              type: 'text',
-              text: conversion_text
+                type: 'text',
+                text: conversion_text
             }
-          ]
-          client.reply_message(event['replyToken'], message)
+        ]
+        client.reply_message(event['replyToken'], message)
       end
     end
   }
